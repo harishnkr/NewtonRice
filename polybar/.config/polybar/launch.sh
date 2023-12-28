fa708821e715a4b4f70b53b50e48f3c646a70106
harishnkr <hari2menon1234@gmail.com>
2023-12-28 09:33:24 +0530

    commented xrandr in xinitrc, edited log message names in polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,11 +4,11 @@ killall -q polybar
 polydir="$HOME/.config/polybar/newton"
 
 echo "---" | tee -a /tmp/polybarws.log /tmp/polybaraction.log /tmp/polybarwin.log /tmp/polybarmus.log /tmp/polybarst.log /tmp/polybardt.log
-polybar -r workspace -c 2>&1 ${polydir}/config.ini | tee -a /tmp/polybarws.log & disown
+polybar -r window -c 2>&1 ${polydir}/config.ini | tee -a /tmp/polybarwindow.log & disown
+polybar -r workspace -c 2>&1 ${polydir}/config.ini | tee -a /tmp/polybarworkspaces.log & disown
 polybar -r actionbar -c 2>&1 ${polydir}/config.ini | tee -a /tmp/polybaraction.log & disown
-polybar -r window -c 2>&1 ${polydir}/config.ini | tee -a /tmp/polybarwin.log & disown
-polybar -r music -c  2>&1 ${polydir}/config.ini | tee -a /tmp/polybarmus.log & disown
-polybar -r statusbar -c  2>&1  ${polydir}/config.ini | tee -a /tmp/polybarst.log & disown
-polybar -r date -c  2>&1 ${polydir}/config.ini | tee -a /tmp/polybardt.log & disown
+polybar -r music -c  2>&1 ${polydir}/config.ini | tee -a /tmp/polybarmusic.log & disown
+polybar -r date -c  2>&1 ${polydir}/config.ini | tee -a /tmp/polybardate.log & disown
+polybar -r statusbar -c  2>&1  ${polydir}/config.ini | tee -a /tmp/polybarstatustray.log & disown
 
 echo "Polybar launched..."
