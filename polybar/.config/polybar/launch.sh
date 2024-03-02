27fb9c15b9c42bbb392a452a48cfeb32f4beb2ae
harishnkr <hari2menon1234@gmail.com>
2024-03-02 13:52:09 +0530

    added mpd scroll script

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,12 +3,12 @@
 killall -q polybar
 polydir="$HOME/.config/polybar/newton"
 
-echo "---" | tee -a /tmp/polybarws.log /tmp/polybaraction.log /tmp/polybarwin.log /tmp/polybarmus.log /tmp/polybarst.log /tmp/polybardt.log
-polybar -r window -c 2>&1 ${polydir}/config.ini | tee -a /tmp/polybarwindow.log & disown
-polybar -r workspace -c 2>&1 ${polydir}/config.ini | tee -a /tmp/polybarworkspaces.log & disown
-polybar -r actionbar -c 2>&1 ${polydir}/config.ini | tee -a /tmp/polybaraction.log & disown
-polybar -r music -c  2>&1 ${polydir}/config.ini | tee -a /tmp/polybarmusic.log & disown
-polybar -r date -c  2>&1 ${polydir}/config.ini | tee -a /tmp/polybardate.log & disown
-polybar -r statusbar -c  2>&1  ${polydir}/config.ini | tee -a /tmp/polybarstatustray.log & disown
+echo "---" | tee -a /tmp/polybarwindow.log /tmp/polybarworkspaces.log /tmp/polybaraction.log  /tmp/polybarmusic.log /tmp/polybardate.log /tmp/polybarstatustray.log 
+polybar -r window -c 2>&1 ${polydir}/config.ini | ts '%H:%M:%S' | tee -a /tmp/polybarwindow.log & disown
+polybar -r workspace -c 2>&1 ${polydir}/config.ini | ts '%H:%M:%S' | tee -a /tmp/polybarworkspaces.log & disown
+polybar -r actionbar -c 2>&1 ${polydir}/config.ini | ts '%H:%M:%S' | tee -a /tmp/polybaraction.log & disown
+polybar -r music -c  2>&1 ${polydir}/config.ini | ts '%H:%M:%S' | tee -a /tmp/polybarmusic.log & disown
+polybar -r date -c  2>&1 ${polydir}/config.ini | ts '%H:%M:%S' | tee -a /tmp/polybardate.log & disown
+polybar -r statusbar -c  2>&1  ${polydir}/config.ini | ts '%H:%M:%S' | tee -a /tmp/polybarstatustray.log & disown
 
-echo "Polybar launched..."
+echo "Polybar launched...."
